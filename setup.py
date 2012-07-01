7b3e08d12629b0ed8e647eff2472b0421262a8a9
Daniel Rocco <drocco@thebrightlink.com>
2012-06-30 22:32:12 -0400

    initial project layout

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='PyPing',
+	version='0.1dev',
+	packages=['pyping', ],
+	license='Creative Commons Attribution-Noncommercial-Share Alike license',
+	long_description=open('README.txt').read(),
+	url='',
+
+    author='Daniel J. Rocco',
+    author_email='drocco@gmail.com'
+)
