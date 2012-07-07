9c0503c5bea401dab936b4376f26790de38c632b
Daniel Rocco <drocco@thebrightlink.com>
2012-07-07 16:49:05 -0400

    first crack at a mock for urlopen

--- tests/test_when_checking_the_availability_of_a_site.py
+++ tests/test_when_checking_the_availability_of_a_site.py
@@ -1,24 +1,55 @@
 import httplib
+from mock import Mock
 
 from pyping.model import Monitor
 
 
-# for this to work, open a shell and run
-#
-#   python -m SimpleHTTPServer
-#
-# or fire up your favorite web server on port 8000
+def mock_opener_director(response_code=httplib.OK):
+	"""Build a mock OpenerDirector instance.
+
+	The returned instance will have an open method returning a response
+	object that mimics the interface of urllib2's responses::
+
+		>>> od = mock_opener_director()
+		>>> url = 'http://google.com'
+		>>> response = od.open(url)
+
+		>>> od.open.assert_called_once_with(url)
+		>>> httplib.OK == response.code
+		True
+
+		>>> response2 = mock_opener_director(httplib.NOT_FOUND).open('http://localhost/bogus')
+		>>> httplib.NOT_FOUND == response2.code
+		True
+
+	"""
+
+	mock_response = Mock(
+		code = response_code
+	)
+
+	open = Mock(return_value=mock_response)
+
+	opener_director = Mock(open=open)
+
+	return opener_director
+
 
 def test_valid_local_http_response_should_yield_positive():
-	monitor = Monitor('http://localhost:8000')
+	opener_director = mock_opener_director()
+	monitor = Monitor('http://localhost:8000', opener_director=opener_director)
+
 	response = monitor.ping()
+
+	opener_director.open.assert_called_once_with('http://localhost:8000')
 
 	assert response
 	assert httplib.OK == response.response_code
 
 
 def test_not_found_should_yield_negative():
-	monitor = Monitor('http://localhost:8000/404.html')
+	opener_director = mock_opener_director(response_code=httplib.NOT_FOUND)
+	monitor = Monitor('http://localhost:8000/404.html', opener_director=opener_director)
 	response = monitor.ping()
 
 	assert not response
@@ -26,7 +57,8 @@ def test_not_found_should_yield_negative():
 
 
 def test_valid_remote_http_response_should_yield_positive():
-	monitor = Monitor('http://google.com')
+	opener_director = mock_opener_director()
+	monitor = Monitor('http://google.com', opener_director=opener_director)
 	response = monitor.ping()
 
 	assert response
